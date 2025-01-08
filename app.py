48c99608ed5f16fec549acc859e003960c7ad371
Saliou DIEDHIOU <133891956+SDKisito@users.noreply.github.com>
2025-01-08 12:36:39 +0100

    app.py

--- app.py
+++ app.py
@@ -4,29 +4,31 @@ import pickle
 
 app = Flask(__name__)
 
-model = pickle.load(open("./ufo-model.pkl", "rb"))
-
+# Load the trained model
+model = pickle.load(open("ufo-model.pkl", "rb"))
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    return render_template("index.html", prediction_text="")
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    # Get user input
+    seconds = request.form['seconds']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
 
-    int_features = [int(x) for x in request.form.values()]
+    # Prepare the input features
+    int_features = [int(seconds), float(latitude), float(longitude)]
     final_features = [np.array(int_features)]
+
+    # Make prediction
     prediction = model.predict(final_features)
 
     output = prediction[0]
-
     countries = ["Australia", "Canada", "Germany", "UK", "US"]
 
-    return render_template(
-        "index.html", prediction_text="Likely country: {}".format(countries[output])
-    )
-
+    return render_template("index.html", prediction_text=f"Likely country: {countries[output]}")
 
 if __name__ == "__main__":
     app.run(debug=True)
